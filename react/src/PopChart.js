8bbd49f0db154eb1cb6bcd3b0e0e1207b4e43da0
Lahiru Kavishka <2020cs095@stu.ucsc.cmb.ac.lk>
2023-08-16 12:22:54 +0530

    changed

---
+++ react/src/PopChart.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+import Chart from 'react-apexcharts';
+
+class PopChart extends Component{
+    render(){
+        return
+    }
+}
+
+export default PopChart;

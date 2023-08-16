d5911cf54e67630ba048e377ce6876142b3f1891
Lahiru Kavishka <2020cs095@stu.ucsc.cmb.ac.lk>
2023-08-16 12:22:24 +0530

    aded pages

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,19 @@
+// src/App.js
+import React from 'react';
+import Data from './data';
+
 function App() {
+  const data = [
+    { label: 'Item 1', value: 10 },
+    { label: 'Item 2', value: 25 },
+    { label: 'Item 3', value: 15 },
+    // Add more data entries as needed
+  ];
   return (
     <div className="App">
-      App 
+      <Data data={data}/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
